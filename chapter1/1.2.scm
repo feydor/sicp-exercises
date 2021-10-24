1c24236f329de883d6b89dc1b785f25c6b3b9685
feydor <vreyes85739@outlook.com>
2021-10-23 21:47:19 -0700

    Finish ex 1.18 to 1.19 and misc fns.

--- chapter1/1.2.scm
+++ chapter1/1.2.scm
@@ -16,17 +16,16 @@
 ; b -> a
 ; c -> b
 (define (fr n)
-  (define (fr-iter a b c n)
-    (if (< n 3)
-        a
-        (fr-iter (+ a (* 2 b) (* 3 c))
-                 a
-                 b
-                 (- n 1))))
-
   (if (< n 3)
       n
       (fr-iter 2 1 0 n)))
+(define (fr-iter a b c n)
+  (if (< n 3)
+      a
+      (fr-iter (+ a (* 2 b) (* 3 c))
+               a
+               b
+               (- n 1))))
 
 
 ; 1.12 - returns the col-th element of the row r of pascal's triangle
@@ -51,7 +50,13 @@
                               b
                               (dec n)))))
 
-; 1.17 iterative multiplication
+; 1.17 recursive multiplication
+(define (multr a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (multr a (halve b))))
+        (else (+ a (multr a (- b 1))))))
+
+; 1.18 iterative multiplication
 (define (mult a b)
   (if (= b 0)
       0
@@ -67,6 +72,52 @@
                 (+ sum a)
                 a
                 (- b 1)))))
+
+; 1.19 fib-iter, O(logn) steps 
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                  b
+                  (+ (square p) (square q))
+                  (+ (* 2 q p) (square q))
+                  (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+; recursive fibonacci
+(define (fibr n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fibr (- n 1)) (fibr (- n 2))))))
+
+; iterative fibonacci
+; a->a+b, b->a when f(0) = 0, f(1) = 1
+; f(n) = a + b
+(define (fibi n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (fibi-iter 0 1 n))))
+(define (fibi-iter a b n)
+  (if (= n 0)
+       a
+       (fibi-iter (+ a b) a (- n 1))))
+
+; halve via succesive subtraction
+; n should be even, otherwise halves the closest even (n - 1)
+(define (half n)
+  (cond ((= n 0) 0)
+        ((odd? n) (half-iter (- n 1) 0))
+        (else (half-iter n 0))))
+(define (half-iter n sub-count)
+  (if (= n sub-count)
+    n
+    (half-iter (- n 1) (+ sub-count 1))))
 
 (define (square n) (* n n))
 (define (inc n) (+ n 1))

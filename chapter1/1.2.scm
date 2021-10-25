506ff288fe02e14402d5e041781a8e049126f7c6
feydor <vreyes85739@outlook.com>
2021-10-24 22:05:19 -0700

    Complete ex 1.21.

--- chapter1/1.2.scm
+++ chapter1/1.2.scm
@@ -119,8 +119,39 @@
     n
     (half-iter (- n 1) (+ sub-count 1))))
 
+; iterative euclid's algorithm for GCD
+; steps: O(logn)
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+; prime? - testing for divisiblity via successive integers starting with 2
+(define (prime? n)
+  (= n (smallest-divisor n)))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  (newline))
+
 (define (square n) (* n n))
 (define (inc n) (+ n 1))
 (define (dec n) (- n 1))
 (define (halve n) (/ n 2))
 (define (double n) (+ n n))
+(define (divides? a b) (= (remainder b a) 0))
+(define (runtime) (tms:clock (times)))
